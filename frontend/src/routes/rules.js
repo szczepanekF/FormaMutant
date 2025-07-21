d93b99c3f03c90f873a6dce0d4b6b265ba6e1ca2
ChristopherGroch <miloszwojtaszczyk@wp.pl>
2025-07-21 23:02:01 +0200

    Initial frontend commit

---
+++ frontend/src/routes/rules.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+
+const Rules = () => {
+  return (
+    <Box p={4}>
+      <Heading as="h1" size="xl">
+        Rules!
+      </Heading>
+    </Box>
+  );
+};
+
+export default Rules;
